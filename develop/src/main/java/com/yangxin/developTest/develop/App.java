9d7b7fd935aec79d6376c992894638474683744d
yangxin <1373219495@qq.com>
2016-05-05 17:05:47 +0800

    提交develop

---
+++ develop/src/main/java/com/yangxin/developTest/develop/App.java
@@ -0,0 +1,13 @@
+package com.yangxin.developTest.develop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
